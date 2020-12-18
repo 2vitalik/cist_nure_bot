fb982834918e45e85cdd1e751170d97ec0c84a2f
Vitaliy Lyapota <2vitalik@gmail.com>
2020-12-18 09:54:43 +0200

    Parse cist CSV (extract: records, groups, subjects)

---
+++ src/cist/parse.py
@@ -0,0 +1,81 @@
+from collections import defaultdict
+
+from shared_utils.io.io import read
+
+import conf
+
+
+def check_unexpected_value(name, value, correct):
+    if value != correct:
+        raise ValueError(f"Unexpected `{name}`: '{value}' != '{correct}'")
+
+
+def parse_csv(group_slug):
+    csv_filename = f'{conf.data_path}/{group_slug}.csv'
+
+    groups = set()
+    subjects = set()
+
+    times = {
+        '07:45:00': 1,
+        '09:30:00': 2,
+        '13:10:00': 3,
+        '11:15:00': 4,
+        '14:55:00': 5,
+        '16:40:00': 6,
+        '18:25:00': 7,
+    }
+
+    data = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+
+    content = read(csv_filename)
+    for line in content.split('\n')[1:]:
+        if not line:
+            continue
+
+        cols = line[1:-1].split('","')
+        title, date_from, time_from, date_to, time_to, \
+            col6, col7, col8, col9, col10, col11, col12, col13 = cols
+
+        if time_from not in times:
+            raise ValueError(f"Unexpected time: {time_from}")
+
+        check_unexpected_value('col6', col6, "Ложь")
+        check_unexpected_value('col7', col7, "Истина")
+        check_unexpected_value('col8', col8, date_from)
+        check_unexpected_value('col10', col10, "2")
+        check_unexpected_value('col11', col11, "Обычная")
+        check_unexpected_value('col13', col13, "Обычный")
+
+        group, rest = title.split(' - ')
+        groups.add(group)
+
+        check_unexpected_value('col12', col12, rest)
+
+        values = rest.split(' ')
+        if len(values) == 5:
+            subject, kind, room, room_2, group_detailed = values
+            subjects.add(subject)
+
+            if not room.endswith(','):
+                raise ValueError(f"Room should end with comma: '{room}'")
+            room = room.rstrip(',')
+
+            data[group][date_from][time_from].append(
+                (subject, kind, f"{room}, {room_2}")
+            )
+        else:
+            parts = rest.split('; ')
+            for part in parts:
+                subject, kind, room, group_detailed = part.split(' ')
+                subjects.add(subject)
+
+                data[group][date_from][time_from].append(
+                    (subject, kind, room)
+                )
+
+    return data, groups, subjects
+
+
+if __name__ == '__main__':
+    parse_csv('pzpi-19')
