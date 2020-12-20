d2a911d185696d0d9cc32a1d192a33f8c319c35a
Vitaliy Lyapota <2vitalik@gmail.com>
2020-12-20 19:31:42 +0200

    Use new prettier kinds

--- src/coda/update.py
+++ src/coda/update.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from src.coda.get import get_subjects_from_coda, get_groups_from_coda, \
     get_records_from_coda
 from src.coda.vars import coda_records, coda_subjects, coda_groups
-from src.utils.slack import slack_status
+from src.data.const import kinds
+from src.utils.slack import slack_status, slack_error
 
 
 def update_coda(potok_slug, new_records, new_groups, new_subjects):
@@ -40,30 +41,27 @@ def update_coda(potok_slug, new_records, new_groups, new_subjects):
                 rows = new_records[group][date_from][time_from]
                 for subject, kind, room in rows:
                     print(' ', subject, kind, room)
-                    kinds = {  # todo: use new kind values
-                        'Лк': 'лк',
-                        'Пз': 'пз',
-                        'Лб': 'лб',
-                        'Зал': 'зал',
-                        'Конс': 'кон',
-                        'ІспКомб': 'ісп',
-                    }
+
+                    new_kind = kinds.get(kind, kind)
+                    if kind == new_kind:
+                        slack_error(f'Unknown kind: {kind}')
+
                     new_record = \
-                        (group, date_coda, time_from, subject, kind, room)
+                        (group, date_coda, time_from, subject, new_kind, room)
                     if new_record in old_records:
                         old_records.remove(new_record)
                         continue
                     slack_status(f'Adding record to coda:\n'
                                  f'• {group}\n'
                                  f'• {date_coda} {time_from}\n'
-                                 f'• {subject}, {kind}, {room}')
+                                 f'• {subject}, {new_kind}, {room}')
                     # todo: send also to telegram group channel
                     coda_records.append({
                         "Группа": group,
                         "Дата": date_coda,
                         "Время": time_from,
                         "Предмет": subject,
-                        "Вид": kind,
+                        "Вид": new_kind,
                         "Ауд": room,
                         "potok_slug": potok_slug,
                         "sys": True,
