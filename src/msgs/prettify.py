d6f82dd9ff5a740299c5fe15deac570ee5599c32
Vitaliy Lyapota <2vitalik@gmail.com>
2021-02-15 06:27:53 +0200

    Bug-fix with getting data for `send_daily`

--- src/msgs/prettify.py
+++ src/msgs/prettify.py
@@ -18,9 +18,9 @@ def prettify_lesson(subject, kind, room, comment, sep=' → '):
 def prettify_time_slot(day_table, time_key):
     lessons = day_table[time_key][::-1]
     number = times.get(time_key, '*️⃣')
-    line = prettify_lesson(*lessons[0])
+    line = prettify_lesson(*lessons[0][1:])
     message = f'{number} <code>{time_key[:5]}</code>: {line}\n'
     for lesson in lessons[1:]:
-        line = prettify_lesson(*lesson)
+        line = prettify_lesson(*lesson[1:])
         message += f'▫️<code>     </code>   {line}\n'
     return message
