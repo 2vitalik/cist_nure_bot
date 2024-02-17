04075b8637a91888243a32a65527471bdf302048
Vitaliy Lyapota <2vitalik@gmail.com>
2024-02-18 01:17:26 +0200

    start_bot: Minor refactoring

--- start_bot.py
+++ start_bot.py
@@ -1,7 +1,8 @@
 import logging
 
 from telegram import Bot
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, \
+    Dispatcher
 from shared_utils.conf import conf as shared_conf
 
 import conf
@@ -9,31 +10,36 @@ from bot.main import MainHandler
 from bot.text import TextHandler
 
 
-def start_bot():
+def enable_logging():
     logging.basicConfig(
         level=logging.DEBUG,
         format='[%(asctime)s] %(levelname)s (%(name)s):  %(message)s'
     )
-    shared_conf.slack_hooks = conf.slack_hooks
 
-    bot = Bot(conf.telegram_token)
-    bot.send_message(chat_id=conf.telegram_admin, text='💬 Starting the bot...')
 
+def add_handles(dispatcher: Dispatcher):
     handler = MainHandler()
     text_handler = TextHandler()
 
-    updater = Updater(token=conf.telegram_token)
-    dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('send', handler.send_cmd))
     dispatcher.add_handler(CommandHandler('text', text_handler.text))
     dispatcher.add_handler(CommandHandler('vote', text_handler.vote))
     dispatcher.add_handler(MessageHandler(Filters.text, handler.default))
     # dispatcher.add_handler(CallbackQueryHandler(handler.process_callback))
 
+
+def start_bot():
+    shared_conf.slack_hooks = conf.slack_hooks
+
+    enable_logging()
+    bot = Bot(conf.telegram_token)
+    bot.send_message(chat_id=conf.telegram_admin, text='💬 Starting the bot...')
+
+    updater = Updater(token=conf.telegram_token)
+    add_handles(updater.dispatcher)
+
     updater.start_polling()
-    print('Bot has successfully started.')
     updater.idle()
-    print('Bot has finished.')
 
 
 if __name__ == '__main__':
